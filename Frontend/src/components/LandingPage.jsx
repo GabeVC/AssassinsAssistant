b5d8d10937142895cd55f8ea36b5b50e10bb8443
GabeC <conneg2@rpi.edu>
2024-10-30 17:47:33 -0400

    Context for existing user login

--- Frontend/src/components/LandingPage.jsx
+++ Frontend/src/components/LandingPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext'
 
 const LandingPage = () => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
 
+  //Check context for already logged in user
+  useEffect(() => {
+    if (!loading && user) {
+      navigate('/home');
+    }
+  }, [user, loading, navigate]);
+
+  if (loading) return <p>Loading...</p>;
+  
   return (
     <div className="landing-page">
       <header>
